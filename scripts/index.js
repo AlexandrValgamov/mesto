61167f6acd7458fee0768d4c7cd4f5628ab4118a
Alexandr Valgamov <valgamov2010@yandex.ru>
2023-07-03 18:51:14 +0500

    add: template/trash

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const editButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__form');
@@ -8,6 +35,9 @@ const subtitleElement = document.querySelector('.profile__subtitle');
 
 const inputNameElement = document.querySelector('.popup__input_edit_name');
 const inputDescriptionElement = document.querySelector('.popup__input_edit_description');
+
+const gallaryElement = document.querySelector('.cards');
+const templateElement = document.querySelector('.card-template');
 
 function editClick() {
   popupElement.classList.add('popup_opened');
@@ -26,6 +56,30 @@ function saveClick(evt) {
   subtitleElement.textContent = inputDescriptionElement.value;
   closeClick();
 }
+
+function createCard({name, link}){
+  const cardElement = templateElement.content.cloneNode(true);
+  const trashElement = cardElement.querySelector('.cards__trash-button');
+
+  cardElement.querySelector('.cards__image').src = link;
+  cardElement.querySelector('.cards__title').textContent = name;
+
+  trashElement.addEventListener('click', function() {
+    trashElement.closest('.cards__card').remove();
+    })
+
+  return cardElement;
+}
+
+function addCard(card){
+  gallaryElement.prepend(card);
+}
+
+
+
+initialCards.forEach((item) => {
+  addCard(createCard(item));
+});
 
 editButtonElement.addEventListener('click', editClick);
 closeButtonElement.addEventListener('click', closeClick);
